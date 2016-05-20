b6037667c525aa778e463b0e3dfe0271c87c40c1
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-05-20 16:16:13 +0100

    added the variables to the js file

---
+++ js.js
@@ -0,0 +1,8 @@
+var dayTime = "linear-gradient(#f1c40f 0%, #d35400 100%)";
+var nightTime = "linear-gradient(#7f8c8d 0%, #2c3e50 76%, #2c3e50 76%)";
+var cancel = "fa fa-ban";
+var noTemp = "0<sup>o</sup>C"; //if the browser doesn't support getting locations
+var lat = ""; //latitude
+var lon = ""; //longitude
+var link = "http://api.openweathermap.org/data/2.5/weather"; //api endpoint
+var key = "c34e05afbe22b8834c31ca9fc05ccb19"; //my api key
